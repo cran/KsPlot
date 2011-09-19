091aeda1846de9b574c763f3143bb04a0a795118
Issei Kurahashi <i.kurahashi@gmail.com>
2011-09-19 00:00:00 +0000

    version 1.2

--- R/KsamplePlot.R
+++ R/KsamplePlot.R
@@ -1,80 +1,127 @@
-KsamplePlot <-
-function(X, y, Ksample = c(seq(40,100,10), 150, seq(200,1000,100)), 
-			Method = "lm", Caret = "No", size = 5, Type = "numeric"){
+KsamplePlot <- function(X, y, Ksample = c(seq(40,100,10), 150, 
+                        seq(200,1000,100)), Method="lm", Caret="No", 
+                        size=5, Type="numeric", NCV=5) {
+  # calculate Error and draw k-sample plot
+  # 
+  # Args: 
+  #   NCV: perform number of harf cross-validation
+  
+  
   library(caTools)
   
-  Rsq <- rep(NA, length(Ksample)*3)
-  for(j in 1:3){
-    for(i in 1:length(Ksample)){
+  RsqTest  <- rep(NA, length(Ksample) * NCV)
+  RsqTrain <- rep(NA, length(Ksample) * NCV)
+  
+  
+  for (j in 1:NCV) {
+    for (i in 1:length(Ksample)) {
       ksample  <- Ksample[i]
       Sample   <- sample(1:nrow(X), ksample)
       KSamplex <- X[Sample, ]
       KSampley <- y[Sample]
       
       
-      #---Half CV
+      # Half CV
       Train     <- sample(1:ksample, ksample / 2)
       Trainx    <- KSamplex[Train, ]
       Trainy    <- KSampley[Train]
-      TrainData <- data.frame(Trainx, y = Trainy)
+      TrainData <- data.frame(Trainx, y=Trainy)
       
       Varidx    <- KSamplex[-Train, ]
       Varidy    <- KSampley[-Train]
-      VaridData <- data.frame(Varidx, y = Varidy)
+      VaridData <- data.frame(Varidx, y=Varidy)
       
       
-      #------numeric
-      if(Type == "numeric"){
-        #---develop model
-        if(Caret == "No"){
-          if(Method == "lm")     Model <- lm(y~., data = TrainData); Varidx <- data.frame(Varidx)
-          if(Method == "svm")    Model <- e1071::svm(y~., data = TrainData)
-          if(Method == "nn")     Model <- nnet::nnet(y~., data = TrainData, size = size, linout = T)
-          if(Method == "rf")     Model <- randomForest::randomForest(y~., data = TrainData)
-          if(Method == "mars")   Model <- mda::mars(Trainx, Trainy)
-          if(Method == "cart")   Model <- mvpart::rpart(y~., data = TrainData)
-          if(Method == "lasso"){
+      # numeric
+      if (Type == "numeric") {
+      
+        # develop model
+        if (Caret == "No") {
+          if(Method == "lm") {
+            Model  <- lm(y~., data = TrainData)
+            Varidx <- data.frame(Varidx)
+          }
+          if (Method == "svm") {
+            Model <- e1071::svm(y~., data = TrainData)
+          }
+          if (Method == "nn") {
+            Model <- nnet::nnet(y~., data = TrainData, size = size, linout = T)
+          }
+          if (Method == "rf") {
+            Model <- randomForest::randomForest(y~., data = TrainData)
+          }
+          if (Method == "mars") {
+            Model <- mda::mars(Trainx, Trainy)
+          }
+          if (Method == "cart") {
+            Model <- mvpart::rpart(y~., data = TrainData)
+          }
+          if (Method == "lasso") {
             cvob1    <- glmnet::cv.glmnet(do.call(cbind, Trainx), Trainy)
             CvLambda <- cvob1$lambda[cvob1$cvm == min(cvob1$cvm)]
-            Model    <- glmnet::glmnet(do.call(cbind, Trainx), Trainy, lambda = CvLambda)
+            Model    <- glmnet::glmnet(do.call(cbind, Trainx), 
+                                       Trainy, lambda = CvLambda)
             Varidx   <- do.call(cbind, Varidx)
           }
         }
-        if(Caret == "Yes"){
-          modeltxt <- parse(text=paste("caret::train(y~., data=TrainData, method= Method)", sep=""))
+        if (Caret == "Yes") {
+          modeltxt <- parse(text=paste("caret::train(y~., data=TrainData, 
+                                                     method= Method)", sep=""))
           Model    <- eval(modeltxt)
         }
         Err    <- as.numeric(Varidy - predict(Model, Varidx))
         MSE    <- Err%*%Err
         Var    <- (Varidy-mean(Varidy))%*%(Varidy-mean(Varidy))
-        Rsq[i + length(Ksample)*(j-1)] <- (Var - MSE)/Var
+        RsqTest[i + length(Ksample) * (j - 1)] <- (Var - MSE) / Var
+        
+        Err1   <- as.numeric(Trainy - predict(Model, Trainx))
+        MSE1   <- Err1%*%Err1
+        Var1   <- (Trainy-mean(Trainy))%*%(Trainy-mean(Trainy))
+        RsqTrain[i + length(Ksample) * (j - 1)] <- (Var1 - MSE1) / Var1
       }
-      #------class
-      if(Type == "binary"){
-        #---develop model
-        if(Caret == "No"){
-          if(Method == "lm"){
+      
+      
+      
+      # class
+      if (Type == "binary") {
+        # develop model
+        if (Caret == "No") {
+          if (Method == "lm") {
             Model <- MASS::lda(y~., data = TrainData)
+            
             Post  <- predict(Model, VaridData)$posterior[, 2]
             ROC   <- caTools::colAUC(Post, Varidy)
-            Rsq[i + length(Ksample)*(j-1)] <- as.numeric(ROC)
+            RsqTest[i + length(Ksample) * (j - 1)] <- as.numeric(ROC)
+            
+            Post1 <- predict(Model, TrainData)$posterior[, 2]
+            ROC1  <- caTools::colAUC(Post1, Trainy)
+            RsqTrain[i + length(Ksample) * (j - 1)] <- as.numeric(ROC1)
           }
-          if(Method == "svm"){
-            Model <- e1071::svm(y ~ ., data = TrainData, kernel="polynomial", degree=3, probability = T)
-            Post  <- predict(Model, VaridData, probability = T)
+          if (Method == "svm") {
+            Model <- e1071::svm(y ~ ., data = TrainData, kernel="polynomial", 
+                                degree=3, probability=T)
+            Post  <- predict(Model, VaridData, probability=T)
             ROC   <- caTools::colAUC(Post, Varidy)
-            Rsq[i + length(Ksample)*(j-1)] <- as.numeric(ROC)
+            RsqTest[i + length(Ksample) * (j - 1)] <- as.numeric(ROC)
+            
+            Post1 <- predict(Model, TrainData, probability=T)
+            ROC1  <- caTools::colAUC(Post1, Trainy)
+            RsqTrain[i + length(Ksample) * (j - 1)] <- as.numeric(ROC1)
           }
         }
-        if(Caret == "Yes"){
+        if (Caret == "Yes") {
         }
       }
     print(paste("Ksample", j, " ", ksample, sep = ""))
     }
   }
-  Ks     <- cbind(Ksample, 1 - Rsq)
-  Ks     <- Ks[(0 <= Ks[, 2] & Ks[, 2] <= 1), ]
-  EstPar <- Ksplot(Ks, Method = Method, Type = Type)
-  list(OneExpPar = EstPar, Ksample = cbind(Ksample, 1 - Rsq))
+  KsTest  <- cbind(Ksample, 1 - RsqTest)
+  KsTest  <- KsTest[(0 <= KsTest[, 2] & KsTest[, 2] <= 1), ]
+  
+  KsTrain <- cbind(Ksample, 1 - RsqTrain)
+  KsTrain <- KsTrain[(0 <= KsTrain[, 2] & KsTrain[, 2] <= 1), ]
+  
+  EstPar <- Ksplot(KsTest, KsTrain, Method=Method, Type=Type)
+  list(OneExpPar = EstPar, Ksample = cbind(Ksample, 1 - RsqTest, 1 - RsqTrain))
 }
 
--- R/Ksplot.R
+++ R/Ksplot.R
@@ -1,34 +1,93 @@
-Ksplot <-
-function(UseData, Type = "numeric", Method){
-  Par    <- data.frame(k = rep(seq(0.01, 1, 0.01), 100), plateau = rep(seq(0.01, 1, 0.01), rep(100, 100)))
-  Object <- rep(NA, 10000)
-  for(i in 1:10000){
-    k         <- Par[i, 1]
-    plateau   <- Par[i, 2]
-    x         <- UseData[, 1]
-    y         <- UseData[, 2]
-    if(Type == "numeric") yhat <- (1   - plateau)*exp(-k*x) + plateau
-    if(Type == "binary")  yhat <- (0.5 - plateau)*exp(-k*x) + plateau
-    Object[i] <- sum((y - yhat)**2)
-  }
-  EstPar <- Par[Object == min(Object), ][1, ]
+Ksplot <- function(UseDataTest, UseDataTrain, Type = "numeric", Method) {
   
-  if(Type == "numeric") plot(UseData, xlim = c(0, max(UseData[, 1])), lwd = 2, ylim=c(0, 1), xlab="K-Sample", ylab="1 - Explained Var", main = paste("Method = ", Method, sep = ""))
-  if(Type == "binary")  plot(UseData, xlim = c(0, max(UseData[, 1])), lwd = 2, ylim=c(0, 0.5), xlab="K-Sample", ylab="1 - AUC", main = paste("Method = ", Method, sep = ""))
-  k       <- as.numeric(EstPar[1])
-  plateau <- as.numeric(EstPar[2])
-  XLim    <- max(UseData[, 1])
-  if(Type == "numeric") Ks.Est <- (1   - plateau)*exp(-k*c(1:XLim)) + plateau
-  if(Type == "binary")  Ks.Est <- (0.5 - plateau)*exp(-k*c(1:XLim)) + plateau
-  lines(c(1:XLim), Ks.Est, lwd = 2)
-  if(Type == "numeric"){ 
-    text(XLim*0.7, 0.8, paste("k (slope):",       round(EstPar[1], digits=2)), adj=0)
-    text(XLim*0.7, 0.7, paste("plateau  :", round(EstPar[2], digits=2)), adj=0)
+  EstParFun <- function(UseData, OptimPar1) {
+    # estimate plateau line
+    # 
+    # Arg:
+    #   UseData: test or train
+    #   OptimPar: 1 or 0
+    
+    Par <- data.frame(k = rep(seq(0.01, 1, 0.01), 100), 
+                      plateau = rep(seq(0.01, 1, 0.01), rep(100, 100)))
+    Object <- rep(NA, 10000)
+    for (i in 1:10000) {
+      k         <- Par[i, 1]
+      plateau   <- Par[i, 2]
+      x         <- UseData[, 1]
+      y         <- UseData[, 2]
+      if (Type == "numeric") {
+        yhat <- (OptimPar1     - plateau) * exp(-k * x) + plateau
+      }
+      if (Type == "binary") {
+        yhat <- (OptimPar1 / 2 - plateau) * exp(-k * x) + plateau
+      }
+      Object[i] <- sum((y - yhat) ^ 2)
+    }
+    Par[Object == min(Object), ][1, ]
   }
-  if(Type == "binary"){ 
-    text(XLim*0.7, 0.45, paste("k (slope):",       round(EstPar[1], digits=2)), adj=0)
-    text(XLim*0.7, 0.4 , paste("plateau  :", round(EstPar[2], digits=2)), adj=0)
+  EstParTest  <- EstParFun(UseDataTest,  1)
+  EstParTrain <- EstParFun(UseDataTrain, 0)
+  
+  # plot
+  if (Type == "numeric") {
+    plot(UseDataTest, xlim=c(0, max(UseDataTest[, 1])), lwd=2, ylim=c(0, 1), 
+         xlab="K-Sample", ylab="1 - Explained Var", 
+         main = paste("Method = ", Method, sep = ""))
+    points(UseDataTrain, pch=0, lwd=2)
   }
-  EstPar
+  if (Type == "binary") {
+    plot(UseDataTest, xlim=c(0, max(UseDataTest[, 1])), lwd=2, ylim=c(0, 0.5), 
+         xlab="K-Sample", ylab="1 - AUC", 
+         main = paste("Method = ", Method, sep = ""))
+    points(UseDataTrain, pch=0, lwd=2)
+  }
+  
+  # curve
+  k       <- as.numeric(EstParTest[1])
+  plateau <- as.numeric(EstParTest[2])
+  
+  k2       <- as.numeric(EstParTrain[1])
+  plateau2 <- as.numeric(EstParTrain[2])
+  
+  XLim    <- max(UseDataTest[, 1])
+  if (Type == "numeric") {
+    Ks.Est  <- (1 - plateau ) * exp(-k  * c(1:XLim)) + plateau 
+    Ks.Est2 <- (0 - plateau2) * exp(-k2 * c(1:XLim)) + plateau2
+  }
+  if (Type == "binary") {
+    Ks.Est  <- (0.5 - plateau ) * exp(-k  * c(1:XLim)) + plateau 
+    Ks.Est2 <- (0   - plateau2) * exp(-k2 * c(1:XLim)) + plateau2
+  }
+  lines(c(1:XLim), Ks.Est , lwd=2)
+  lines(c(1:XLim), Ks.Est2, lwd=2, lty=2)
+  
+  # legend
+  if (Type == "numeric") {
+    text(XLim * 0.7, 0.8, paste("Test  plateau  :", 
+                                round(EstParTest[2], digits=2)), adj=0)
+    text(XLim * 0.7, 0.76,  paste("Test  k (slope):", 
+                                round(EstParTest[1], digits=2)), adj=0)
+    text(XLim * 0.7, 0.7, paste("Train plateau  :", 
+                                round(EstParTrain[2], digits=2)), adj=0)
+    text(XLim * 0.7, 0.66,  paste("Train k (slope):", 
+                                round(EstParTrain[1], digits=2)), adj=0)
+    legend("topright", c("Test", "Train"), pch=c(1, 0), lty=1:2, lwd=2)
+  }
+  if (Type == "binary") {
+    text(XLim * 0.7, 0.44 , paste("plateau  :", 
+                                 round(EstParTest[2], digits=2)), adj=0)
+    text(XLim * 0.7, 0.42, paste("k (slope):", 
+                                 round(EstParTest[1], digits=2)), adj=0)
+    text(XLim * 0.7, 0.39, paste("Train plateau  :", 
+                                round(EstParTrain[2], digits=2)), adj=0)
+    text(XLim * 0.7, 0.37,  paste("Train k (slope):", 
+                                round(EstParTrain[1], digits=2)), adj=0)
+    legend("topright", c("Test", "Train"), pch=c(1, 0), lty=1:2, lwd=2)
+  }
+  
+  # output
+  list(EstParTest, EstParTrain)
 }
+
+
 
